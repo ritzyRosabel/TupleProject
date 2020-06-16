f00f56b416f5ddf4c710b3d6ed358ceb23954bc9
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-16 23:58:36 +0100

    limitation of tuple

--- TupleProject/Program.cs
+++ TupleProject/Program.cs
@@ -13,6 +13,11 @@ namespace TupleProject
         //When you want to return multiple values from a method without using ref or out parameters.
         //When you want to pass multiple values to a method through a single parameter.
         //When you want to hold a database record or some values temporarily without creating a separate class.
+        //limitation of tuple
+        //Tuple is a reference type and not a value type. It allocates on heap and could result in CPU intensive operations.
+        //Tuple is limited to include 8 elements. You need to use nested tuples if you need to store more elements. However, this may result in ambiguity.
+        //Tuple elements can be accessed using properties with a name pattern Item<elementNumber> which does not make sense.
+
         static void Main(string[] args)
         {
             var person = Tuple.Create(12, "Miracle", "Oshey");

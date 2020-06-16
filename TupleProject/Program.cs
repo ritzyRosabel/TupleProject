b1a69da458309f24a6d002d2bec1c769329d5d94
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-16 23:50:45 +0100

    Tuple used as method parameter

--- TupleProject/Program.cs
+++ TupleProject/Program.cs
@@ -10,6 +10,15 @@ namespace TupleProject
     {
         static void Main(string[] args)
         {
+            var person = Tuple.Create(12, "Miracle", "Oshey");
+            DisplayResult(person);
+            Console.ReadLine();
+        }
+
+        private static void DisplayResult(Tuple<int, string, string> person)
+        {
+            Console.WriteLine($"{person.Item2} {person.Item3} is {person.Item1} years old");
+
         }
     }
 }

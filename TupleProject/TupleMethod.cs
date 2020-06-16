603a44d21fefa4447ec244dde3a87498741103e2
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-16 23:26:24 +0100

    8 elements per instance

--- TupleProject/TupleMethod.cs
+++ TupleProject/TupleMethod.cs
@@ -16,6 +16,7 @@ namespace TupleProject
             Tuple <int, string, string> tuple = new Tuple<int, string, string>(1, "Kemi", "Kolade");
             var sample = Tuple.Create<int, string>(22, "age");//Tuple static helper class
             var samples = Tuple.Create(224, "aged");//Tuple static helper class2
+            var sample3 = Tuple.Create(1, 2, 3, 4, 5, 6, 7, 8);// can only take 8 elements per instance. creates a compile time error if more than 8
                 return tuple;
             //yet to find out the return type to use when i waant to return a Tuple
         }

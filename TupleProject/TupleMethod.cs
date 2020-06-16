f2501d588497c781e48c183a0d009d7a859cf362
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-16 23:36:22 +0100

    access  to elements in tuple

--- TupleProject/TupleMethod.cs
+++ TupleProject/TupleMethod.cs
@@ -17,6 +17,17 @@ namespace TupleProject
             var sample = Tuple.Create<int, string>(22, "age");//Tuple static helper class
             var samples = Tuple.Create(224, "aged");//Tuple static helper class2
             var sample3 = Tuple.Create(1, 2, 3, 4, 5, 6, 7, 8);// can only take 8 elements per instance. creates a compile time error if more than 8
+            _ = sample.Item1;
+            _ = sample.Item2;//use Item1-7 to access elements in the tuple use Rest for the 8th element if up to eight.
+            _ = sample3.Item1;
+            _ = sample3.Item2;
+            _ = sample3.Item3;
+            _ = sample3.Item4;
+            _ = sample3.Item5;
+            _ = sample3.Item6;
+            _ = sample3.Item7;
+            _ = sample3.Rest;
+
                 return tuple;
             //yet to find out the return type to use when i waant to return a Tuple
         }

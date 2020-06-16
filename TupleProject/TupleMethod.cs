97252b67cec48fa590a954fa41a3a9e89c545a26
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-06-16 23:19:41 +0100

    Tuple Constructor

---
+++ TupleProject/TupleMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TupleProject
+{
+    public class TupleMethod
+    {
+        public static Object  TupleM()
+        {
+            int count = 3;
+            string colors = "green white green";
+            Tuple <int, string, string> tuple = new Tuple<int, string, string>(1, "Kemi", "Kolade");
+                return tuple;
+            //yet to find out the return type to use when i waant to return a Tuple
+        }
+    }
+}
